8b2fc263e89cd38664f1f9abde627a925db965c6
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2022-04-22 18:45:20 +0100

    Hyperparameter PPO Update

--- Agents/Simple_Satellite/SBL.py
+++ Agents/Simple_Satellite/SBL.py
@@ -2,11 +2,28 @@
 import gym
 import SimpleSatellite
 
-env =  gym.make("SimpleSatellite-v0")
-
 # import agent
-from stable_baselines3.ppo import PPO
+from stable_baselines3 import PPO
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("ppo_SimpleSatellite_v0")
+kwarg = { 'learning_rate': 1e-3, 'n_steps':2048, 'batch_size':64, 'n_epochs':5, 'gamma':0.99, 'gae_lambda':0.95,
+         'clip_range':0.2, 'clip_range_vf':None, 'ent_coef':0.0, 'vf_coef':0.5, 'max_grad_norm':0.5, 
+         'target_kl':None,  'policy_kwargs': dict(net_arch=[dict(pi=[64, 64], vf=[64, 64])])}
+n_timestep = 25000
+if __name__ == '__main__':
+    save_dir = "SBL_Logs/"
+
+    # Create the vectorized environment
+    env = env =  gym.make("SimpleSatellite-v0")
+
+    # Create model  
+    policy = "MultiInputPolicy"
+
+    # Create model
+    
+    model = PPO(policy, env, verbose=1, tensorboard_log='./'+save_dir+"tensorboard/", **kwarg)
+
+    # Train model
+    model.learn(total_timesteps=n_timestep)
+
+    #Save Model
+    model.save(save_dir+'ppo_SimpleSatellite_v0')
