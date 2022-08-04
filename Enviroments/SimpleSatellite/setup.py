2689993bfd3cb4d5ed94126f3b98d7a8b4fd9d99
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2022-08-04 09:46:05 +0100

    gym version setup update

--- Enviroments/SimpleSatellite/setup.py
+++ Enviroments/SimpleSatellite/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='SimpleSatellite', 
     version='0.0.1', 
     description='Minimalistic observation satellite environment',
-    install_requires=['gym', 'pygame'],
+    install_requires=['gym==0.20.0', 'pygame'],
     packages=['SimpleSatellite', 'SimpleSatellite.envs'])
