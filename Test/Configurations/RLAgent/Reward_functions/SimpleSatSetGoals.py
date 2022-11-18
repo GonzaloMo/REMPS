e6076f8ddc76a482bdf5e67249d349334c4abd11
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2022-11-18 11:56:26 +0000

    Reward_v2 creation

--- Test/Configurations/RLAgent/Reward_functions/SimpleSatSetGoals.py
+++ Test/Configurations/RLAgent/Reward_functions/SimpleSatSetGoals.py
@@ -79,3 +79,65 @@ def Reward_v1(env: gym.Env, action_in: Tuple[int,int]):
             reward += .001
 
     return reward
+
+def Reward_v2(env: gym.Env, action_in: Tuple[int,int]):
+    reward = 0 
+    # Get action and observation
+    obs = env.get_obs()
+    goals = obs["Goals"]
+    action, img = action_in
+    check_action, add_info = env.SatSim.check_action(action,img)
+    # Reward for taking a correct action
+    if check_action:
+        if action == SatelliteSim.ACTION_TAKE_IMAGE:
+            # Reward for taking a picture
+            reward += 1
+            # Reward for taking a picture of a goal
+            if goals[img-1] > 1:
+                reward += 5
+        if action == SatelliteSim.ACTION_ANALYSE:
+            # Reward for analysing a picture
+            reward += 1
+            # Reward for analysing a picture of a goal
+            if goals[img-1] > 1:
+                reward += 5
+        if action == SatelliteSim.ACTION_DUMP:
+            # Reward for dumping a picture
+            reward += 1
+            # Reward for dumping a picture of a goal
+            if goals[img-1] > 1:
+                reward += 50
+            # Reward for dumping all pictures
+            all_complete = False
+            for obs_goal in goals:
+                if obs_goal < 0:
+                    all_complete = True
+                    break
+            if all_complete:
+                reward += 10000
+    else:
+        # Action that made action to not be executed
+        if add_info == "Memory full":
+            reward -= 10
+        elif add_info == "Not above target":
+            reward -= 100
+        elif add_info == "Not in light":
+            reward -= 1
+        elif add_info == "No image to analyse":
+            reward -= 10
+        elif add_info == "No image to dump":
+            reward -= 10
+        elif add_info == "Not above GS":
+            reward -= 100
+        elif add_info == "Satellite busy":
+            reward -= 100
+        
+    # Power 
+    if env.SatSim.POWER_OPTION:
+        if obs["Power"] < 25.:
+            reward -= 10
+        elif obs["Power"] > 99.:
+            reward -= .1
+        else:
+            reward += .001
+    return reward
