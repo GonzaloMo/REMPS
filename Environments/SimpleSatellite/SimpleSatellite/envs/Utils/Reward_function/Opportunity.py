689cb342e9db3a259c2931703d71f970fee0d412
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-01-13 12:06:34 +0000

    Reward v2 opp

--- Environments/SimpleSatellite/SimpleSatellite/envs/Utils/Reward_function/Opportunity.py
+++ Environments/SimpleSatellite/SimpleSatellite/envs/Utils/Reward_function/Opportunity.py
@@ -18,3 +18,15 @@ def Reward_v1(env: gym.Env, action_in: Tuple[int,int]) -> float:
         action != env.SatSim.ACTION_DO_NOTHING): # action is not do nothing
             return -.0001
     return 0
+
+def Reward_v2(env: gym.Env, action_in: Tuple[int,int]) -> float:
+    action, img = action_in
+    obs = env.get_obs()
+    check, _ = env.SatSim.check_action(action, img)
+    if action == env.SatSim.ACTION_TAKE_IMAGE and check: # Picture of the opportunity taken
+        return 1
+    if action == env.SatSim.ACTION_ANALYSE and check: # Opportunity analysed
+        return 1
+    if action == env.SatSim.ACTION_DUMP and check:  # Opportunity dumped
+        return 1
+    return 0
