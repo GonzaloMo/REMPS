7471b36a24a1084905e624ead7d9aed07744631a
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-01-23 10:15:21 +0000

    reward V2 opp v2

--- Environments/SimpleSatellite/SimpleSatellite/envs/Utils/Reward_function/Opportunity/V2.py
+++ Environments/SimpleSatellite/SimpleSatellite/envs/Utils/Reward_function/Opportunity/V2.py
@@ -36,19 +36,14 @@ def Reward_v2(env: gym.Env, action_in: Tuple[int,int]) -> float:
     check, add_info = env.SatSim.check_action(action, img)
     # Rewards
     if action == env.SatSim.ACTION_TAKE_IMAGE and check: # Picture of the opportunity taken
-        reward += 100
+        reward += 1000
     if action == env.SatSim.ACTION_ANALYSE and check: # Opportunity analysed
-        reward += 100
+        reward += 1000
     if action == env.SatSim.ACTION_DUMP and check:  # Opportunity dumped
-        reward += 100
-    
-    
+        reward += 1000
     #Penalties
     if env.SatSim.POWER_OPTION:
-        # Penalize for having less tahn 10% of power
-        if obs["Power"] < 10.:
-            reward -= 10
         # Penalize for more than 99% of power
-        elif obs["Power"] < 0.1:
-            reward -= 1000
+        if obs["Power"] < 0.1:
+            reward -= 100000
     return reward
