4681a6304aaf0980ac1adc9b142c5976771b3c34
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-01-23 10:13:30 +0000

    update envcreator

--- Agents/RL-Agent/RLAgent/Utils/ray.py
+++ Agents/RL-Agent/RLAgent/Utils/ray.py
@@ -4,6 +4,24 @@ import gym
 import os
 import ray.tune as tune
 from typing import Dict, Any, List, Optional, Tuple, Union
+current_dir = os.getcwd()
+def env_creator_old(env_config: Dict={"env": "SimpleSatellite-v0", "Env_setup": ["./Configurations/Environment_Config/Env_1.yaml"], 
+            "Reward_Module": "Reward_functions.SimpleSat", "Reward_Function": "Reward_v1", "Log_dir": "./Results/Sim/"}):
+    # Import Reward function
+    import importlib
+    module_name = env_config["Reward_Module"]
+    Rewards = importlib.import_module(module_name, package=None)
+    config = {}
+    for env_config_file in env_config["Env_setup"]:
+        env_config_file = env_config_file.replace("./", f"{current_dir}/")
+        import yaml
+        with open(env_config_file, "r") as f:
+            config.update(yaml.load(f, Loader=yaml.FullLoader))
+    config["Reward"] = getattr(Rewards, env_config["Reward_Function"])
+    config["Log_dir"] = env_config["Log_dir"]
+    MyEnv = gym.make(env_config["env"], **config)
+    return MyEnv  # return an env instance
+
 def env_creator(env_config: Dict={"env": "SimpleSatellite-v0", "Env_setup": {}, 
             "Reward_Module": "Reward_functions.SimpleSat", "Reward_Function": "Reward_v1", "Log_dir": "./Results/Sim/"}):
     # Import Reward function
