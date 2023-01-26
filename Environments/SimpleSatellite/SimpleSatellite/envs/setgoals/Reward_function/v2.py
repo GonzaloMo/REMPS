1d15f00b645a99b5f0bd182c6a67852357f1205b
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-01-26 09:25:24 +0000

    reward v2 setgoals v2

--- Environments/SimpleSatellite/SimpleSatellite/envs/setgoals/Reward_function/v2.py
+++ Environments/SimpleSatellite/SimpleSatellite/envs/setgoals/Reward_function/v2.py
@@ -85,9 +85,76 @@ def Reward_v1(env: gym.Env, action_in: Tuple[int,int]):
         elif add_info == "Satellite busy":
             reward -= 100
         pass
+    # # Incentivise having not having the memory free
+    # if obs["Memory Level"] > 0.1:
+    #     reward += min(obs["Memory Level"][0], env.SatSim.MEMORY_SIZE*.5) * reward
+    # Power 
+    if env.SatSim.POWER_OPTION:
+        if obs["Power"] < 25.:
+            reward -= 100
+        elif obs["Power"] < 0.01:
+            reward = -1000000000000
+        elif obs["Power"] < 100. and \
+                env.SatSim.check_light()>0 and \
+                env.SatSim.Taking_action == SatelliteSim.ACTION_DO_NOTHING:
+            reward += 1
+    return reward
+
+
+########
+# penalty for taking to long to achieve goals
+
+def Reward_v1(env: gym.Env, action_in: Tuple[int,int]):
+    reward = 0 
+    
+    # Get action and observation
+    obs = env.get_obs()
+    pos = cossin_2_degrees(obs["Pos"][0], obs["Pos"][1])
+    goals = env.goals
+    limit_orbits = np.sum(env.initial_goals)/4
+
+    action, img = action_in
+    check_action, add_info = env.SatSim.check_action(action,img)
+    # Reward for taking a correct action
+    if check_action:
+        if action == SatelliteSim.ACTION_TAKE_IMAGE:
+            # Reward for taking a picture of a goal
+            if goals[img-1] > 0:
+                reward += 1000
+        if action == SatelliteSim.ACTION_ANALYSE:
+            # Reward for analysing a picture of a goal
+            if goals[img-1] > 0:
+                reward += 400
+        if action == SatelliteSim.ACTION_DUMP:
+            # Reward for dumping a picture of a goal
+            if goals[img-1] > 1:
+                reward += 500
+            if goals[img-1] == 1:
+                reward += 100000
+    else:
+        # Action that made action to not be executed
+        if add_info == "Memory full":
+            reward -= 10
+        elif add_info == "Not above target":
+            reward -= 10
+        elif add_info == "Not in light":
+            reward -= 1
+        elif add_info == "No image to analyse":
+            reward -= 10
+        elif add_info == "No image to dump":
+            reward -= 10
+        elif add_info == "Not above GS":
+            reward -= 10
+        elif add_info == "Satellite busy":
+            reward -= 100
+        pass
+
+    ## penalty for taking to long to achieve goals
+    if env.SatSim.orbits > limit_orbits:
+        reward -= 10**(env.SatSim.orbits-limit_orbits/4)
     # Incentivise having not having the memory free
-    if obs["Memory Level"] > 0.1:
-        reward += min(obs["Memory Level"][0], env.SatSim.MEMORY_SIZE*.5) * reward
+    # if obs["Memory Level"] > 0.1:
+    #     reward += min(obs["Memory Level"][0], env.SatSim.MEMORY_SIZE*.5) * reward
     # Power 
     if env.SatSim.POWER_OPTION:
         if obs["Power"] < 25.:
