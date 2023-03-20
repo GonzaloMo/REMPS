f288be796642849e3453a99ce2f3801ad5a21c35
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-03-20 14:21:07 +0000

    Rtest2 sg2

--- Environments/SimpleSatellite/SimpleSatellite/envs/setgoals/Reward_function/v2.py
+++ Environments/SimpleSatellite/SimpleSatellite/envs/setgoals/Reward_function/v2.py
@@ -1,5 +1,6 @@
 from SimpleSatellite.envs.simulation.v1_old import SatelliteSim
 from typing import Dict, List, Tuple, Union
+from copy import deepcopy
 import gym
 import numpy as np
 def cossin_2_degrees(x: float, y: float) -> float:
@@ -36,6 +37,45 @@ def Reward_test(env: gym.Env, action_in: Tuple[int,int]):
     if env.SatSim.POWER_OPTION:
         if obs["Power"] < 0.01:
             reward = -100000
+    return reward
+
+def Reward_test_2(env: gym.Env, action_in: Tuple[int,int]):
+    reward = 0
+    # Get action and observation
+    obs = env.get_obs()
+    goals = env.goals
+    action, img = action_in
+    check_action, add_info = env.SatSim.check_action(action,img)
+    goals_after_action = deepcopy(goals)
+    # Reward for taking a correct action
+    if check_action:
+        if action == SatelliteSim.ACTION_TAKE_IMAGE:
+            # Reward for taking a picture of a goal
+            if goals[img-1] > 0:
+                reward += .001
+        if action == SatelliteSim.ACTION_ANALYSE:
+            # Reward for analysing a picture of a goal
+            if goals[img-1] > 0:
+                reward += .002 
+        if action == SatelliteSim.ACTION_DUMP:
+            # Reward for dumping a picture of a goal
+            if goals[img-1] > 0:
+                reward += .1
+                goals_after_action[img-1] -= 1
+            
+    if np.sum(goals_after_action) == 0:
+        reward += 100
+    # Negative reward per step 
+    reward -= 0.1/env.SatSim.period
+    if env.SatSim.POWER_OPTION:
+        if (obs["Power"]*100) < 25:
+            reward -= .001
+        if (obs["Power"]*100) < 1:
+            reward -= 100
+    if obs["Memory Level"] > 0.9:
+        reward -= .001
+    elif obs["Memory Level"] > 0.5:
+        reward -= .0001
     return reward
 
 #####
