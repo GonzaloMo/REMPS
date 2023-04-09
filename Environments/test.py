3a9ed839dd89f7dc0178a003c00445df53fe71ee
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-04-09 16:33:51 +0100

    test

--- Environments/test.py
+++ Environments/test.py
@@ -39,20 +39,8 @@ env.quit()
 
 curses.endwin()
 
-
-# CV_path = [
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_0.yaml",
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_1.yaml",
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_2.yaml",
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_3.yaml",
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_4.yaml",
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_5.yaml",
-#     "./SimpleSatellite/SimpleSatellite/envs/setgoals/Configurations/curriculum_learning/Dif_6.yaml",
-# ]
-# env = gym.make('SimpleSatellite-setgoals-CV', config_files={"main_config_file": Mainconfigfile, "CV_path": CV_path, "Reward_Function": "Reward_v1"})
 # for i in range(0,4):
 #     env.set_task(i)
-#     # print(env.difficulty_config[i])
 #     epi_percentage = []
 #     rewards = []
 #     for epi in tqdm(range(100)):
@@ -61,12 +49,5 @@ curses.endwin()
 #         while not done:
 #             action = env.action_space.sample()
 #             observation, reward, done, info = env.step(action)
-
 #             env.render()
-#             # print(f"target_visibility:{env.SatSim.above_target}")
-#             # print(f"GS visibilty: {env.SatSim.above_gs}")
-#             # obs = env.SatSim.get_state()
 #             print_obs(observation)
-#             # print_obs(obs, stp=1)
-#         rewards.append(env.Total_reward)
-#         epi_percentage.append((1- np.sum(env.goals)/np.sum(env.initial_goals)))
