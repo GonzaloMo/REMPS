dbdb59a996fe5975a3d7acca5496c0670910c1de
GonzaloMo <gonzalo.montesino-valle@strath.ac.uk>
2023-07-13 13:05:39 +0100

    Rewards Modification Plann following

--- Environments/SimpleWorld/SimpleWorld/envs/planfollowing/Reward_function/v0.py
+++ Environments/SimpleWorld/SimpleWorld/envs/planfollowing/Reward_function/v0.py
@@ -2,41 +2,10 @@ from typing import Dict, List, Tuple, Union
 import gym
 import numpy as np
 
-def Reward_v0(env, action_in):
+def Reward_0(env, action_in):
     # Get action and observation
-    reward = 0
-    grid_size = env.sim.grid_size
-    obs = env.get_obs()
-    Map = np.reshape(obs["Map"], (grid_size, grid_size))
-    Map_plan = np.reshape(obs["Planner_Map"], (grid_size, grid_size))
-    next_pos = env.next_plan_state
-    dpos = env.action_pos_dict[action_in]
-    pos = env.pos
-    x, y = pos
-    blocked = False
-    x_n, y_n = np.clip(pos + dpos, 0, grid_size-1)
-    if x_n < 0 or x_n >= grid_size or y_n < 0 or y_n >= grid_size:
-        blocked = True
-        reward =  -1
-    if Map[x_n][y_n] == env.sim.obstacleTag:
-        blocked = True
-        reward =  -1
-        
-    elif Map[x_n][y_n] == env.sim.goalPositionTag:
-        reward = 20
-    else:
-        reward = 0
-    X_nn, Y_nn = next_pos
-    if Map_plan[x_n][y_n] == env.sim.goalPositionTag and not blocked:
-        if X_nn == x_n and Y_nn == y_n:
-            reward += 2
-        reward += 1
     info = {}
-    return reward, info
-
-def Reward_v1(env, action_in):
-    # Get action and observation
-    reward = 0.01
+    reward = 0.
     grid_size = env.sim.grid_size
     obs = env.get_obs()
     Map = np.reshape(obs["Map"], (grid_size, grid_size))
@@ -44,25 +13,48 @@ def Reward_v1(env, action_in):
     next_pos = env.next_plan_state
     dpos = env.action_pos_dict[action_in]
     pos = env.pos
-    x, y = pos
-    blocked = False
     x_n, y_n = pos + dpos
     if x_n < 0 or x_n >= grid_size or y_n < 0 or y_n >= grid_size:
-        blocked = True
-        reward =  -1
+        return -1, info
     x_n, y_n = np.clip(pos + dpos, 0, grid_size-1)
     if Map[x_n][y_n] == env.sim.obstacleTag:
-        blocked = True
-        reward =  -1
+        return -1, info
         
-    elif Map[x_n][y_n] == env.sim.goalPositionTag:
-        reward = 20
-    else:
-        reward = 0
+    if Map[x_n][y_n] == env.sim.goalPositionTag:
+        return 20, info
     X_nn, Y_nn = next_pos
-    if Map_plan[x_n][y_n] == env.sim.goalPositionTag and not blocked:
+    if Map_plan[x_n][y_n] == env.sim.goalPositionTag:
         if X_nn == x_n and Y_nn == y_n:
-            reward += 2
-        reward += 1
-    info = {"x_n": x_n, "y_n": y_n, "blocked": blocked, "reward": reward, "next_pos": next_pos, "pos": pos, "dpos": dpos, "Map": Map, "Map_plan": Map_plan, "action": action_in}
+            return 5, info
+        return 1, info
+    # {"x_n": x_n, "y_n": y_n, "blocked": blocked, "reward": reward, "next_pos": next_pos, "pos": pos, "dpos": dpos, "Map": Map, "Map_plan": Map_plan, "action": action_in}
+    return reward, info
+
+def Reward_1(env, action_in):
+    # Get action and observation
+    info = {}
+    reward = -0.01
+    grid_size = env.sim.grid_size
+    obs = env.get_obs()
+    Map = np.reshape(obs["Map"], (grid_size, grid_size))
+    Map_plan = np.reshape(obs["Planner_Map"], (grid_size, grid_size))
+    next_pos = env.next_plan_state
+    dpos = env.action_pos_dict[action_in]
+    pos = env.pos
+    x, y = pos
+    x_n, y_n = pos + dpos
+    if x_n < 0 or x_n >= grid_size or y_n < 0 or y_n >= grid_size:
+        return -1, info
+    x_n, y_n = np.clip(pos + dpos, 0, grid_size-1)
+    if Map[x_n][y_n] == env.sim.obstacleTag:
+        return -1, info
+        
+    if Map[x_n][y_n] == env.sim.goalPositionTag:
+        return 20, info
+    X_nn, Y_nn = next_pos
+    if Map_plan[x_n][y_n] == env.sim.goalPositionTag:
+        if X_nn == x_n and Y_nn == y_n:
+            return 5, info
+        return 1, info
+    # {"x_n": x_n, "y_n": y_n, "blocked": blocked, "reward": reward, "next_pos": next_pos, "pos": pos, "dpos": dpos, "Map": Map, "Map_plan": Map_plan, "action": action_in}
     return reward, info
